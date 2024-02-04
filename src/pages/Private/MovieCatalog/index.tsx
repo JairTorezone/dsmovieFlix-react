bd227811a7ccd2a867b1c5570bcbb9ef383883dd
Jair Torezone <jairtorezone@gmail.com>
2024-02-04 20:13:17 -0300

    created page movieCatalog

--- src/pages/Private/MovieCatalog/index.tsx
+++ src/pages/Private/MovieCatalog/index.tsx
@@ -2,8 +2,17 @@ import "./styles.css";
 
 const MovieCatalog = () => {
   return (
-    <div>
-      <h1>Ola mundo</h1>
+    <div className="movie-catalog-container">
+      <h2>Tela listagem de filmes</h2>
+
+      <ul>
+        <li>
+          <a href="/">Acessar /movies/1</a>
+        </li>
+        <li>
+          <a href="/">Acessar /movies/1</a>
+        </li>
+      </ul>
     </div>
   );
 };
