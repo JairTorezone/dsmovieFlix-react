143d94ca4f159dc645c40a68d023fb15eda5465b
Jair Torezone <jairtorezone@gmail.com>
2024-02-06 17:33:02 -0300

    created MovieDetails

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import Navbar from "components/Navbar";
 import { ReactComponent as HomeImage } from "assets/images/home.svg";
 import Button from "components/Button";
 
--- src/pages/Private/MovieDetails/index.tsx
+++ src/pages/Private/MovieDetails/index.tsx
@@ -1,7 +1,33 @@
+import Button from "components/Button";
+
+import "./styles.css";
+import Card from "components/Card";
+
 function MovieDetails() {
   return (
-    <div>
-      <h1>Pg details</h1>
+    <div className="moviedetails-container">
+      <h2>Tela detalhes do filme id: 1</h2>
+
+      <div className="moviedetails-form">
+        <form>
+          <div className="form-wrapper">
+            <input
+              type="text"
+              name="assessment"
+              placeholder="Deixe sua avaliação aqui"
+            />
+          </div>
+          <Button title="SALVAR AVALIAÇÃO" />
+        </form>
+      </div>
+
+      <div className="moviedetails-info-cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
